8eb327f38792a9a004f60088182f2bb8bc5c2334
Paolo Di Ciaula <pdiciaula@pic-informatica.it>
2016-12-26 00:53:51 +0100

    Icaro Development
    * Add dynamic config of server and proxy settings
    * Now you can connect more machine to nginx with "addr" field
    * Add "config_profile" that permits to configure proxy roles for each api
    
    TODO:
    * Auto destroy of include server line in nginx config
    * Manage the apis already on when i refresh icaro
    * Start to do benchmarks

--- run_apis.rb
+++ run_apis.rb
@@ -1,38 +1,14 @@
 require 'timeout'
 require 'socket'
 
-api_environ = ARGV[0]+"_API"
-apis = ENV[api_environ].split(";")
+api = ARGV[0]
+port = ARGV[1]
 
-def is_port_open?(ip, port)
-    begin
-        Timeout::timeout(1) do
-            begin
-                s = TCPSocket.new(ip, port)
-                s.close
-                return true
-            rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-                return false 
-            end
-        end
-    rescue Timeout::Error
-    end
-    return false
-end
-def executeApi(api,port)        
-    cmd = "uwsgi --socket 127.0.0.1:"+port.to_s+" --protocol=http --wsgi-file api/"+api+".py --callable api" 
+def executeApi(api,port)    
+    cmd = "uwsgi --socket 127.0.0.1:" + port.to_s + " --protocol=http --wsgi-file api/" + api + ".py --callable api" 
     fork{
         exec(cmd)
     }
 end
 
-
-i = 0
-port = ""
-apis.each do |api|        
-    begin
-        port = ("800"+i.to_s).to_i
-        i+=1    
-    end while is_port_open?("127.0.0.1", port)
-    executeApi(api, port)     
-end    
+executeApi(api, port)
