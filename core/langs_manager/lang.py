78dfad3c419c781a497f015070fdd788548d74fe
Paolo Di Ciaula <pdiciaula@pic-informatica.it>
2017-05-13 17:31:32 +0200

    Icaro Development add run_commmand option in lang configuration

--- core/langs_manager/lang.py
+++ core/langs_manager/lang.py
@@ -9,4 +9,9 @@ class Lang:
         self.lib_manager = self.lang["library_manager"]
         self.requirements_file = json.loads(utils.readLines(utils.selfLocation()+"/langs_manager/library_manager.json"))[self.lib_manager]["requirements_file"]
 
-
+    def run_command(self, port, path, name):
+        cmd = self.lang["run_command"]
+        cmd.replace("{port}", port)
+        cmd.replace("{path}", path)
+        cmd.repalce("{name}", name)
+        return cmd
